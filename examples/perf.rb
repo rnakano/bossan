9524393611e4ef1befefd677d75b74fbdc4bf2fc
NAKANO Ryusuke <rsk.nakano@gmail.com>
2013-02-25 18:07:31 +0900

    Impl some options for examples/perf.rb
    Add -s, -n and -c options.

--- examples/perf.rb
+++ examples/perf.rb
@@ -4,7 +4,24 @@ require 'net/http'
 
 KNOWN_SERVERS = [:webrick, :thin, :bossan]
 START_PORT = 8000
-servers = KNOWN_SERVERS.dup
+N = 1000
+C = 1
+
+servers = [:webrick, :bossan]
+n = N
+c = C
+
+opt = OptionParser.new
+opt.on("-s Servers", "list of servers (default: webrick,bossan)"){|s|
+  servers = s.split(",").map(&:downcase).map(&:intern) & KNOWN_SERVERS
+}
+opt.on("-n TotalRequests", "number of total requests (default: 1000)"){|reqs|
+  n = reqs.to_i
+}
+opt.on("-c Connections", "number of total connections (default: 1)"){|cons|
+  c = cons.to_i
+}
+opt.parse!(ARGV)
 
 class MyApp
   def call env
@@ -38,18 +55,21 @@ def wait
   sleep 0.5
 end
 
-def send_requests addr, port, n
-  n.times {
-    Net::HTTP.start(addr, port){|http|
-      response = http.get("/")
-      raise "Body error" unless response.body != "hi"
+def send_requests addr, port, n, c
+  c.times.map{
+    Thread.start{
+      (n/c).times {
+        Net::HTTP.start(addr, port){|http|
+          response = http.get("/")
+          raise "Body error" unless response.body != "hi"
+        }
+      }
     }
-  }
+  }.each{|t| t.join}
 end
 
 Net::HTTP.version_1_2
 
-N = 1000
 ports = (START_PORT...(START_PORT+servers.size))
 servers.zip(handlers, ports).map{|s, handler, port|
   pid = fork do
@@ -59,7 +79,7 @@ servers.zip(handlers, ports).map{|s, handler, port|
   Process.detach(pid)
   wait
   timer = Time.now
-  send_requests("localhost", port, N)
+  send_requests("localhost", port, n, c)
   dt = Time.now - timer
   Process.kill(:INT, pid)
   dt
